29964b2c12ad1b011bdeac3299ce7917068b196e
JPietrucha89 <98188400+JPietrucha89@users.noreply.github.com>
2023-11-12 11:59:49 +0100

    imported csv in order to create and show df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents\' New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -8,3 +9,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# import CSV file from web in order to make df
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
