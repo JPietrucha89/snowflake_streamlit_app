0bda38d38e7afa7c9585c1ce87c3ae8fd35314f8
JPietrucha89 <98188400+JPietrucha89@users.noreply.github.com>
2023-11-13 21:10:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ if st.button("Get Fruit Load List"):
 def check_if_fruit_exists_in_snowflake_table(new_fruit):
   with my_cnx.cursor() as my_cur: 
     my_cur.execute( "select count(*) from fruit_load_list where fruit_name = '" + new_fruit + "' )" )
-    count_of_new_fruit_in_table = cast(my_cur.fetchone() as number)
+    count_of_new_fruit_in_table = my_cur.fetchone()
     if count_of_new_fruit_in_table > 0: 
       result = False 
     else: 
