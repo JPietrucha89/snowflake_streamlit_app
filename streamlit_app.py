a21efff4c5d408b71c84e4c810d01c41b6240e89
JPietrucha89 <98188400+JPietrucha89@users.noreply.github.com>
2023-11-17 17:16:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ if st.button("Get Fruit List"):
 # allow user to add fruit to the list
 def check_if_fruit_exists_in_snowflake_table(new_fruit):
   with my_cnx.cursor() as my_cur: 
-    my_cur.execute( "select count(*) from fruit_load_list where fruit_name = '" + new_fruit + "' )" )
+    my_cur.execute( "select count(*) from fruit_load_list where fruit_name = '" + new_fruit + "'" )
     count_of_new_fruit_in_table = my_cur.fetchone()
     if count_of_new_fruit_in_table > 0: 
       result = False 
@@ -86,16 +86,10 @@ my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 
 #st.text(check_if_fruit_exists_in_snowflake_table("banana"))
 with my_cnx.cursor() as my_cur: 
-    my_cur.execute( "select count(*) from fruit_load_list where fruit_name = '" + "banana" + "' )" )
+    my_cur.execute( "select count(*) from fruit_load_list where fruit_name = 'banana' " )
     count_of_new_fruit_in_table = my_cur.fetchone()
     st.text(count_of_new_fruit_in_table)
   
-#st.text(check_if_fruit_exists_in_snowflake_table("jackfruit"))
-with my_cnx.cursor() as my_cur: 
-    my_cur.execute( "select count(*) from fruit_load_list where fruit_name = '" + "jackfruit" + "' )" )
-    count_of_new_fruit_in_table = my_cur.fetchone()
-    st.text(count_of_new_fruit_in_table)
-
 
 add_my_fruit = st.text_input("Which fruit would you like to add?", "jackfruit")
 if st.button("Add a Fruit to the List"):
