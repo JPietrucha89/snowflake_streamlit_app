420fc645bb04ceb82d7acfcd4d08c551a2117b60
JPietrucha89 <98188400+JPietrucha89@users.noreply.github.com>
2023-11-17 17:22:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 #st.text(check_if_fruit_exists_in_snowflake_table("banana"))
 with my_cnx.cursor() as my_cur: 
     my_cur.execute( "select count(*) from fruit_load_list where fruit_name = 'banana' " )
-    count_of_new_fruit_in_table = my_cur.fetchone()
+    count_of_new_fruit_in_table = my_cur.fetchone()[0]
     st.text(count_of_new_fruit_in_table[0])
 ## testing 
 
