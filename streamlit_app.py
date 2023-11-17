6a3f375f2fa0c89e687cf76f6ee857ca519fe92b
JPietrucha89 <98188400+JPietrucha89@users.noreply.github.com>
2023-11-17 17:49:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,7 +86,7 @@ def insert_row_to_snowflake_table(new_fruit):
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 #st.text(check_if_fruit_exists_in_snowflake_table("banana"))
 with my_cnx.cursor() as my_cur: 
-    my_cur.execute( "select count(*) from fruit_load_list where fruit_name = 'banana' " )
+    my_cur.execute( "select count(*) from fruit_load_list where fruit_name = 'jackfruit' " )
     count_of_new_fruit_in_table = my_cur.fetchone()
     int_count = int(count_of_new_fruit_in_table[0])
     st.write('Number of occurencies: ', int_count)
